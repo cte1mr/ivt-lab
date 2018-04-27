f023224f12ad0f66e2812029705389a1126a5715
cte1mr <38343836+cte1mr@users.noreply.github.com>
2018-04-27 00:10:10 -0700

    Fixing enum compare

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -37,7 +37,7 @@ public class GT4500 implements SpaceShip {
 
         boolean firingSuccess = false;
 
-        if (firingMode == SINGLE){
+        if (firingMode == FiringMode.SINGLE){
                 if (wasPrimaryFiredLast) {
                     // try to fire the secondary first
                     if (!secondaryTorpedoStore.isEmpty()) {
